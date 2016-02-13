bde8f54756aa8b6fcc95a1457e8d22888055bf3e
Jim Chiang <jchiang@slac.stanford.edu>
2016-02-12 20:57:16 -0800

    handle case where the eups setup has already been run

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,12 +1,15 @@
 import subprocess
 
-# Modify the setup/setup.sh script to setup the requested Stack
+# Modify the setup/setup.sh script to set up the requested Stack
 # packages using the most recent tags.
 setup_script = open('setup/setup.sh', 'a')
 for package in "{{cookiecutter.stack_packages}}".split():
-    command = 'source setup/setup.sh; eups list | grep %s | tail -1' % package
-    tag = subprocess.check_output(command, shell=True, 
-                                  executable='/bin/bash').split()[-1]
+    command = 'source setup/setup.sh; eups list %s | tail -1' % package
+    listing = subprocess.check_output(command, shell=True, 
+                                      executable='/bin/bash').split()
+    while listing[-1] == 'setup':
+        listing.pop()
+    tag = listing[-1]
     setup_script.write('setup %(package)s -t %(tag)s\n' % locals())
 setup_script.write('''eups declare {{cookiecutter.repo_name}} -r . -c
 setup {{cookiecutter.repo_name}}\n''')
